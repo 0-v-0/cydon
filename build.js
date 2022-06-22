63474dd295e9880113b935d6fd563aab22b297b0
0-v-0 <kduice@qq.com>
2022-06-22 08:38:00 +0800

    add build script

--- build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { compileTS, runTask } from 'mefes';
-
-runTask({
-	compile: () => compileTS({ entryPoints: ['src/cydon.ts'], outdir: 'dist' }),
-	release: () => compileTS({ entryPoints: ['src/cydon.ts'], outdir: 'dist', minify: true })
-}, "compile");
