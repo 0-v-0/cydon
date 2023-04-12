d69d701c9b991613d1362d880b1ed1fcbed1bdbe
0-v-0 <kduice@qq.com>
2023-04-12 13:56:26 +0800

    release

--- packages/cydon/vite.config.js
+++ packages/cydon/vite.config.js
@@ -13,16 +13,7 @@ export default defineConfig({
 			ecma: 2020,
 			compress: {
 				ecma: 2020,
-				passes: 3,
-				unsafe: true,
-				unsafe_arrows: true,
-				unsafe_comps: true,
-				unsafe_Function: true,
-				unsafe_math: true,
-				unsafe_methods: true,
-				unsafe_proto: true,
-				unsafe_regexp: true,
-				unsafe_undefined: true
+				unsafe: true
 			}
 		}
 	}
